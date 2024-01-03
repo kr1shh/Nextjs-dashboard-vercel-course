961f75d94ad1c424a1d22cb4e85213e23c537c04
Binnukrishna <binnukrishna5233@gmail.com>
2024-01-03 11:50:51 +0530

    authentication added

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 
 const FormSchema = z.object({
@@ -34,6 +36,27 @@ export type State = {
         status?:string[]
     }
     message?:string | null
+}
+
+
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData
+){
+    try{
+        await signIn( 'credentials',formData )
+    }catch( err ){
+        if( err instanceof AuthError ){
+            switch( err.type ){
+                case 'CredentialsSignin':
+                    return 'Invalid credintial'
+                default:
+                    return 'Something went wrong.'
+            }
+        }
+        throw err
+    }
 }
 
 
